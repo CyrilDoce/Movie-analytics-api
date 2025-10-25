f1afd8e4c13325d86fa38a506a3b9154f85e016c
= <cyridoce21@gmail.com>
2025-10-25 08:35:20 +0100

    configured the CRUD endpoints with limited functionality

--- app/app.py
+++ app/app.py
@@ -8,12 +8,27 @@ app = Flask(__name__)
 def home():
     return jsonify({"welcome to the app": 'home'})
 
-@app.route("/summary")
+#READ
+@app.route("/summary", methods=['GET'])
 def summary():
     data = load_data()
     summary = get_summary(data)
     return jsonify(summary)
-    
+
+#CREATE
+@app.route("/insert", methods=['PUT'])     
+def insert():
+    pass
+
+#DELETE
+@app.route("/delete",methods = ['DELETE'])
+def delete():
+    pass
+
+#UPDATE
+@app.route("/update", methods = ['UPDATE'])
+def update():
+    pass
 
 if __name__ == "__main__":
     app.run( host="0.0.0.0", port=5000, debug=True)
